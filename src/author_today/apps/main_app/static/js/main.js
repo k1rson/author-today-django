c8f1ae7a813cb4a9927e5aca555e1d3289aee5d3
k1rson <vlad2k15@bk.ru>
2023-06-17 14:51:54 +0300

    [update] update home page

--- src/author_today/apps/main_app/static/js/main.js
+++ src/author_today/apps/main_app/static/js/main.js
@@ -9,3 +9,28 @@ function() {
 function OpenModalForSearch(){
     alert('gg')
 }
+
+function OpenModalNotCompletedElement() {
+    const modal = document.createElement('div');
+    modal.classList.add('modal', 'fade');
+    modal.id = 'modalnotcompletedelement';
+
+    modal.innerHTML = `
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+         <h5 class="modal-title centered-modal-title">Ошибка перехода</h5>
+        </div>
+        <div class="modal-body">
+            <p>К сожалению, пока что не сделали :(</p>
+        </div>
+        </div>
+    </div>
+    `;
+
+    document.body.appendChild(modal);
+
+    $('#modalnotcompletedelement').modal('show');
+}
+  
+  
